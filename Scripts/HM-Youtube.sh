fb1c8263c6bd6cbc8b71b03aa901a3e4074fa102
Ha1derMirza <x7and7@gmail.com>
2021-11-15 22:39:40 +0000

    Begining the scripting process of Hm-Youtube.sh

---
+++ Scripts/HM-Youtube.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# _   _ __  __
+#| | | |  \/  |
+#| |_| | |\/| | Made by Haider Mirza
+#|  _  | |  | | My Github: https://github.com/Ha1derMirza
+#|_| |_|_|  |_|
+
+# NOTE: Dependencies: libnotify, dunst, dmenu.
+# Dunst and libnotify are optional if you remove line 23
